c34ae52b6e9cef7c88048ab1f0b01f171ebfe029
Mohammed Fahmy <mohamed.fahmy@linkdev.com>
2025-03-17 23:12:43 +0200

    Done with user[id] route

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,20 +1,29 @@
 import {PrismaClient, Prisma} from "@prisma/client";
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
-    const {id} =  await params;
-    const prismaClient = new PrismaClient();
-    const user = await prismaClient.users.findUnique({where : {id: +id}})
-    prismaClient.$disconnect()
-    if(!user) return NextResponse.json({error: 'User not found', status: 401});
-    return NextResponse.json({user, status: 200})
+export async function GET({params}: { params: Promise<{ id: string }> }) {
+    try {
+        const {id} = await params;
+        const prismaClient = new PrismaClient();
+        const user = await prismaClient.users.findUnique({where: {id: +id}})
+        prismaClient.$disconnect()
+        if (!user) return NextResponse.json({error: 'User not found', status: 404});
+        return NextResponse.json({user, status: 200})
+    } catch (err) {
+        return NextResponse.json({error: err, status: 401});
+    }
+
 }
-export async function PUT(){
-    const prismaClient = new PrismaClient();
 
-   //TODO
-    // const {id} =  await params;
-
-    // const prismaClient = new PrismaClient();
-    // const data = await req.json();
+export async function PUT(req: NextRequest, {params}: { params: Promise<{ id: string }> }) {
+    try {
+        const data: Prisma.usersUpdateInput = (await req.json()) satisfies Prisma.usersUpdateInput;
+        const {id} = await params;
+        const prismaClient = new PrismaClient();
+        const updatedUser = await prismaClient.users.update({where: {id: +id}, data: data});
+        prismaClient.$disconnect()
+        return NextResponse.json({updatedUser, status: 200})
+    } catch (err) {
+        return NextResponse.json({error: err, status: 401});
+    }
 }
