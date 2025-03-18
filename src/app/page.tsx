cea5451a16bfa3b43cc0736a360b621667be18ea
Mohammed Fahmy <mohamed.fahmy@linkdev.com>
2025-03-18 02:58:10 +0200

    Project done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import Link from "next/link";
 import {Button} from "@/components/ui/button";
 
 export default async function Home() {
-    const {users, error} : { users: User[], error:  string } = await fetch(process.env.URL + "/api/user").then(res => {
+    const {users, error}: { users: User[], error: string } = await fetch(process.env.URL + "/api/user").then(res => {
         if (res.status === 200) return res.json()
         return {error: res.json()};
     }).catch((err) => {
         return {error: err}
     });
-    if(error) return <div className={"bg-amber-700 text-white"}>Oops! Something went wrong! Try refreshing...</div>
+    if (error) return <div className={"bg-amber-700 text-white"}>Oops! Something went wrong! Try refreshing...</div>
     return (
         <div className={"flex flex-col justify-center items-center min-h-96 py-10"}>
             <Card className={"bg-slate-600 text-white rounded-lg shadow-sm w-5/6 py-8 border-0 overflow-clip"}>
@@ -21,21 +21,22 @@ export default async function Home() {
                 </CardHeader>
                 <CardContent className={"max-h-96 items-center overflow-x-scroll w-full"}>
                     <div className={"grid grid-rows-3 grid-flow-col m-auto "}>
-                    {(users as User[]).slice(0, 50).map((user) => (
-                        <Avatar key={user.id} className={"w-18 h-18 rounded-full m-3"}>
-                            <AvatarImage src={`https://i.pravatar.cc/150?u=${user.email}`}/>
-                            <AvatarFallback>{user.first_name.at(0)??"" + user.last_name?.at(0)}</AvatarFallback>
-                        </Avatar>
-                    ))}
+                        {(users as User[]).slice(0, 50).map((user) => (
+                            <Avatar key={user.id} className={"w-18 h-18 rounded-full m-3"}>
+                                <AvatarImage src={`https://i.pravatar.cc/150?u=${user.email}`}/>
+                                <AvatarFallback>{user.first_name.at(0) ?? "" + user.last_name?.at(0)}</AvatarFallback>
+                            </Avatar>
+                        ))}
                     </div>
                 </CardContent>
                 <CardFooter className={"flex gap-2 sm:gap-8 w-full justify-center"}>
                     <Link href="/">
-                        <Button variant="outline" size="default" className={"text-black cursor-pointer hover:bg-slate-300"}>
+                        <Button variant="outline" size="default"
+                                className={"text-black cursor-pointer hover:bg-slate-300"}>
                             Add User
                         </Button>
                     </Link>
-                    <Link href="/">
+                    <Link href="/users">
                         <Button size="default" className={"cursor-pointer hover:bg-slate-700"}>
                             View All Users
                         </Button>
---
+++ src/app/users/page.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import {
+    NavigationMenu, NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
+import {Card, CardContent} from "@/components/ui/card";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {User} from "@/app/types";
+
+export default async function Users() {
+    const {users, error}: { users: User[], error: string } = await fetch(process.env.URL + "/api/user").then(res => {
+        if (res.status === 200) return res.json()
+        return {error: res.json()};
+    }).catch((err) => {
+        return {error: err}
+    });
+    if (error) return <div className={"bg-amber-700 text-white"}>Oops! Something went wrong! Try refreshing...</div>
+    return (
+        <div>
+            <nav
+                className="fixed rounded-b-lg py-4 inset-x-0 top-0 z-50 bg-slate-400 opacity-40 text-black shadow-sm translate-y-0 md:translate-y-0/2 dark:bg-gray-950">
+                <div className="container px-4 md:px-6">
+                    <NavigationMenu>
+                        <NavigationMenuList className="flex w-full gap-4 items-center">
+                            <div>Here are your users in all their glory!</div>
+                            <NavigationMenuItem>
+                                <Link legacyBehavior passHref
+                                      href="/">
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Home
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <Link legacyBehavior passHref
+                                      href="/">
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Add User
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+            </nav>
+            <div className="container mx-auto py-32 px-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {users.map((user) => (
+                        <Card key={user.id} className="overflow-scroll bg-slate-500 text-white border-0">
+                            <CardContent className="p-6">
+                                <div className="flex items-center space-x-4">
+                                    <Avatar className="h-12 w-12">
+                                        <AvatarImage src={`https://i.pravatar.cc/150?u=${user.email}`}/>
+                                        <AvatarFallback>{user.first_name.at(0) ?? "" + user.last_name?.at(0)}</AvatarFallback>
+                                    </Avatar>
+                                    <div className="space-y-1">
+                                        <h3 className="font-medium">{`${user.first_name} ${user.last_name}`}</h3>
+                                        <p className="text-sm text-white">{user.email}</p>
+                                    </div>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
